e31d5857399c33cb8819b987150d3f32b21e4e8c
Barnabas Bulpett <barnabasbulpett@gmail.com>
2023-11-25 20:21:12 -0500

    Initial Commit

---
+++ Projects/java/PixelArtGenerator/src/test/java/com/barnabasbulpett/PixelArtGenerator/PixelArtGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barnabasbulpett.PixelArtGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelArtGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
