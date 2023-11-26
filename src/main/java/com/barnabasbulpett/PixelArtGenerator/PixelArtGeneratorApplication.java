01a9b23361589313071bb2ecee361f44257a8af8
Barnabas Bulpett <barnabasbulpett@gmail.com>
2023-11-25 23:19:01 -0500

    Fix up dependencies

--- src/main/java/com/barnabasbulpett/PixelArtGenerator/PixelArtGeneratorApplication.java
+++ src/main/java/com/barnabasbulpett/PixelArtGenerator/PixelArtGeneratorApplication.java
@@ -1,7 +1,14 @@
 package com.barnabasbulpett.PixelArtGenerator;
 
+import net.coobird.thumbnailator.Thumbnails;
+import net.coobird.thumbnailator.resizers.Resizers;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 @SpringBootApplication
 public class PixelArtGeneratorApplication {
